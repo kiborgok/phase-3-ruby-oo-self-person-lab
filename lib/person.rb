ba13b58d1537f59203623eea7ef11a8b169daf2b
Alex Kiborgok <borgokalek14@gmail.com>
2022-06-10 21:11:25 +0300

    commit tree 67e9b238cb7402fe6329d354d96b8e4b7c0c5e20

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,70 @@
 # your code goes here
+require("pry")
+class Person
+    attr_accessor :bank_account, :hygiene
+    attr_reader :name, :happiness
+    def initialize(name, bank_account=25,happiness=8,hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(happiness)
+        if happiness > 10
+            @happiness = 10
+        elsif happiness < 1
+            @happiness = 0
+        else
+            @happiness = happiness
+        end
+    end
+
+    def hygiene=(hygiene)
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 1
+            @hygiene = 0
+        else
+            @hygiene = hygiene
+        end
+    end
+
+    def happy?
+        if self.happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if self.hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(person)
+        person.happiness += 3
+    end
+end
+
+# binding.pry
